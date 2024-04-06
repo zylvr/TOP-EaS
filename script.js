e9cf61100ce555c8f282aa630fc2bce9c9e1731f
zylvr <136989038+zylvr@users.noreply.github.com>
2024-04-06 15:46:23 -0600

    calculate grid max height
    grid max height is now calculated from siblings height and body (margin and padding) height so when new elements are added it doesn't have to be adjusted manually

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function getAbsoluteHeight(el) {
+    // Get the DOM Node if you pass in a string
+    el = (typeof el === 'string') ? document.querySelector(el) : el; 
+
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles['marginTop']) +
+                 parseFloat(styles['marginBottom']);
+
+    return Math.ceil(el.offsetHeight + margin);
+}
+
+
+
+
+
+// DETERMINE GRID MAX HEIGHT
+
+const root = document.documentElement;
+// select body and all children except grid
+const bodyAndChildrenNoGrid = document.querySelectorAll("body, body > *:not(.grid)");
+const body = document.querySelector("body");
+let combinedHeight = 0;
+
+bodyAndChildrenNoGrid.forEach(el => { // get total height of them
+    combinedHeight += getAbsoluteHeight(el);
+});
+
+// remove body height to only have body margins
+combinedHeight = combinedHeight - document.querySelector("body").offsetHeight;
+
+// add body padding
+combinedHeight = combinedHeight + parseFloat(window.getComputedStyle(body).getPropertyValue("padding-top"));
+combinedHeight = combinedHeight + parseFloat(window.getComputedStyle(body).getPropertyValue("padding-bottom"));
+
+root.style.setProperty("--non-grid-combined-height", `${combinedHeight}px`);
+
+
+
+
+
 let fragment = new DocumentFragment();
 
 for (let i = 0; i < 16 * 16; i++) {
